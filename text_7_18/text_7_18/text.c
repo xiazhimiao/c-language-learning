449b5c21c1ef8fca076d3148e0182bf0d7a45d8a
xiazhimiao <3309686397@qq.com>
2023-07-19 07:52:57 +0800

    复习提交代码到远程仓库

---
+++ text_7_18/text_7_18/text.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int a1 = 0;
+	int a2 = 0;
+	scanf("%d %d", &a1, &a2);
+	//int sum = a1 + a2;
+	printf("%d\n",a1+a2);
+	return 0;
+}
