f06e5f3733a0174901adbf0c0ecb7a073d761487
xiazhimiao <3309686397@qq.com>
2023-07-23 21:39:12 +0800

    日常

--- text_7_23_二分查找/text_7_23_二分查找/text.c
+++ text_7_23_二分查找/text_7_23_二分查找/text.c
@@ -3,22 +3,22 @@
 int main()
 {
 	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int n = 14;
+	int n = 4;
 	int sz = sizeof(arr) / sizeof(arr[0]);
 	int zuo = 0;
 	int you = sz + 1;
-	int zhong = (zuo + you) / 2;
 	while (zuo<=you)
 	{
+		//如果有序数组很大，这个位置“zuo+you”的值可能溢出int范围
+		int zhong = (zuo + you) / 2;
+		//另一种写法：int zhong = zuo + (you - zuo) / 2;
 		if (arr[zhong] > n)
 		{
 			you = you - 1;
-			zhong = (zuo + you) / 2;
 		}
 		else if (arr[zhong] < n)
 		{
 			zuo = zuo + 1;
-			zhong = (zuo + you) / 2;
 		}
 		else
 		{
---
+++ text_7_23_关机程序/text_7_23_关机程序/text.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+	char arr[20] = { 0 };
+	system("shutdown -s -t 180");
+again:
+	printf("温馨提示：关闭本程序起不到取消关机的目的\n请输入赵陈浩大傻逼，否则电脑将在180s后关机:");
+	scanf("%s",arr);
+	if (strcmp(arr, "赵陈浩大傻逼") == 0)
+	{
+		system("shutdown -a");
+	}
+	else
+	{
+		goto again;
+	}
+	return 0;
+}
