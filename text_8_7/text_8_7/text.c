bba8ad9200ba9b2ffd2fa0ed7e59b065e6109c68
xiazhimiao <3309686397@qq.com>
2023-08-07 20:09:54 +0800

    日常学习

---
+++ text_8_7/text_8_7/text.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int arr[3][3] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	//二维数组初始化
+//	for (i = 0; i < 3; ++i)
+//	{
+//		for (j = 0; j < 3; ++j)
+//		{
+//			if (i == 1 && j == 1)
+//				arr[i][j] = 1;
+//			else
+//				arr[i][j] = 0;
+//		}
+//	}
+//	//打印二维数组
+//	for (i = 0; i < 3; ++i)
+//	{
+//		for (j = 0; j < 3; ++j)
+//		{
+//			printf("%d", arr[i][j]);
+//		}
+//		printf("\n");
+////	}
+//	return 0;
+//
+//}
+int main()
+{
+	int m = 0;
+	int n = 1;
+	int* p1 = &m;
+	int* p2 = &n;
+	
+	//*p1 = *p2;
+	
+
+	return 0;
+}
